a31d33fa1d5f93951a4ce73af5d837268d963e32
Delonteee <davide.gasparotti@tutanota.com>
2024-02-22 13:31:56 +0100

    Esercizio completo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Laravel Primi Passi</title>
+    </head>
+    <body>
+        
+        <main>
+            <h1>
+                Hello World!!!
+            </h1>
+            <p>
+                Made with Laravel
+            </p>
+        </main>
+    
+    </body>
+</html>
